433996c42ed710502ddef564cfd03be02f96433e
stanley <tochukwuu14@gmail.com>
2022-04-05 13:42:16 +0100

    Fix lint errors

--- app.js
+++ app.js
@@ -4,26 +4,23 @@ const closemenu = document.querySelector('.close-menu');
 const openmenu = document.querySelector('.open-menu');
 
 function toggleMenu() {
-    if (menu.classList.contains('showMenu')) {
-      menu.classList.remove('showMenu');
-      closemenu.style.display = 'none';
-      openmenu.style.display = 'block';
-    } 
-    else {
-      menu.classList.add('showMenu');
-      closemenu.style.display = 'block';
-      openmenu.style.display = 'none';
-    }
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closemenu.style.display = 'none';
+    openmenu.style.display = 'block';
+  } else {
+    menu.classList.add('showMenu');
+    closemenu.style.display = 'block';
+    openmenu.style.display = 'none';
   }
-  
-  closeham.addEventListener('click', toggleMenu);
-  
-  const menuLinks = document.querySelectorAll('.mobileLink');
-  
-  menuLinks.forEach(
-    (mobileLink) => {
-      mobileLink.addEventListener('click', toggleMenu);
-    },
-  );
-  
-  
+}
+
+closeham.addEventListener('click', toggleMenu);
+
+const menuLinks = document.querySelectorAll('.mobileLink');
+
+menuLinks.forEach(
+  (mobileLink) => {
+    mobileLink.addEventListener('click', toggleMenu);
+  },
+);
