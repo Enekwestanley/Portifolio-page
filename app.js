973febbcb08081b1503d05b3c34b302622d30924
stanley <tochukwuu14@gmail.com>
2022-04-05 10:18:26 +0100

    Add javascript code for the harmbuger

---
+++ app.js
@@ -0,0 +1,28 @@
+const menu = document.querySelector('.mobile-menu');
+const closeham = document.querySelector('.close-ham');
+const closemenu = document.querySelector('.close-menu');
+const openmenu = document.querySelector('.open-menu');
+
+function toggleMenu() {
+    if (menu.classList.contains('showMenu')) {
+      menu.classList.remove('showMenu');
+      closemenu.style.display = 'none';
+      openmenu.style.display = 'block';
+    } else {
+      menu.classList.add('showMenu');
+      closemenu.style.display = 'block';
+      openm.style.display = 'none';
+    }
+  }
+  
+  closeham.addEventListener('click', toggleMenu);
+  
+  const menuLinks = document.querySelectorAll('.mobileLink');
+  
+  menuLinks.forEach(
+    (mobileLink) => {
+      mobileLink.addEventListener('click', toggleMenu);
+    },
+  );
+  
+  
