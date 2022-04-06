24cdb4e8c37090645a746d2dc0c9e1c1a0ae5d6d
stanley <tochukwuu14@gmail.com>
2022-04-06 22:53:35 +0100

    Modify some html code Modify some css code Modify some javascript code

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const menu = document.querySelector('.mobile-menu');
 const closeham = document.querySelector('.close-ham');
 const closemenu = document.querySelector('.close-menu');
 const openmenu = document.querySelector('.open-menu');
+const header = document.getElementById('logo');
 const projs = document.querySelectorAll('.work');
 
 function toggleMenu() {
@@ -33,7 +34,8 @@ const modalObj = {
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       img: 'img/Portfolio1-big.svg',
       tech: ['html', 'css', 'javaScript'],
-      source: 'https://github.com/Enekwestanley/Portifolio-page',
+      live: 'https://chukwuebukavictor.github.io/My-portfolio/',
+      source: 'https://github.com/chukwuebukaVictor/My-portfolio',
     },
     modal2: {
       title: 'Multi-Post Stories',
@@ -41,7 +43,8 @@ const modalObj = {
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       img: 'img/Portfolio2-big.svg',
       tech: ['html', 'css', 'javaScript'],
-      source: 'https://github.com/Enekwestanley/Portifolio-page',
+      live: 'https://chukwuebukavictor.github.io/My-portfolio/',
+      source: 'https://github.com/chukwuebukaVictor/My-portfolio',
     },
     modal3: {
       title: 'Tonic',
@@ -49,7 +52,8 @@ const modalObj = {
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       img: 'img/Portfolio3.svg',
       tech: ['html', 'css', 'javaScript'],
-      source: 'https://github.com/Enekwestanley/Portifolio-page',
+      live: 'https://chukwuebukavictor.github.io/My-portfolio/',
+      source: 'https://github.com/chukwuebukaVictor/My-portfolio',
     },
     modal4: {
       title: 'Multi-Post Stories',
@@ -57,12 +61,11 @@ const modalObj = {
       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       img: 'img/Portfolio4-big.svg',
       tech: ['html', 'css', 'javaScript'],
-      source: 'https://github.com/Enekwestanley/Portifolio-page',
+      live: 'https://chukwuebukavictor.github.io/My-portfolio/',
+      source: 'https://github.com/chukwuebukaVictor/My-portfolio',
     },
   };
-
   
-  const main = document.querySelector('.main');
   const modalSubt = document.getElementById('modalSubt');
   const modalTech = document.getElementById('modalTech');
   
@@ -86,7 +89,7 @@ const modalObj = {
       otherDiv.innerHTML = i;
     });
   }
-
+  
   function addTechs(modal) {
     modalObj[modal].tech.forEach((i) => {
       const li = document.createElement('li');
@@ -120,42 +123,35 @@ const modalObj = {
   mBtn1.onclick = function seeP1() {
     popModal('modal1');
     modal.style.display = 'block';
-    projOn();
   };
   
   mBtn2.onclick = function seeP2() {
     popModal('modal2');
     modal.style.display = 'block';
-    projOn();
   };
   
   mBtn3.onclick = function seeP3() {
     popModal('modal3');
     modal.style.display = 'block';
-    projOn();
   };
   
   mBtn4.onclick = function seeP4() {
     popModal('modal4');
     modal.style.display = 'block';
-    projOn();
   };
-
+  
   function closeModal() {
-    modal.style.display = "none";
-    projOff();
+    modal.style.display = 'none';
   }
-
+  
   xp.onclick = function x() {
     closeModal();
     clearModal();
   };
-
+  
   window.onclick = function clickOut(event) {
     if (event.target === modal) {
       closeModal();
       clearModal();
     }
   };
-  
-  
